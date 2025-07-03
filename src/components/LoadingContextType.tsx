685d3b41f8f2665d013337f23cb09f99409924a4
sujithrajendran <67055680+sujithrajendran@users.noreply.github.com>
2025-07-03 15:29:03 +0530

    Create LoadingContextType.tsx

---
+++ src/components/LoadingContextType.tsx
@@ -0,0 +1,26 @@
+import { createContext, useContext, useState, ReactNode } from "react";
+
+interface LoadingContextType {
+  isLoading: boolean;
+  setIsLoading: (val: boolean) => void;
+}
+
+const LoadingContext = createContext<LoadingContextType | undefined>(undefined);
+
+export const LoadingProvider = ({ children }: { children: ReactNode }) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  return (
+    <LoadingContext.Provider value={{ isLoading, setIsLoading }}>
+      {children}
+    </LoadingContext.Provider>
+  );
+};
+
+export const useLoading = () => {
+  const context = useContext(LoadingContext);
+  if (!context) {
+    throw new Error("useLoading must be used within a LoadingProvider");
+  }
+  return context;
+};

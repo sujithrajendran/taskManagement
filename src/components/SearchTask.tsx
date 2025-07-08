910861808ad7bfc82661747bfb01103ba0e90e01
sujithrajendran <67055680+sujithrajendran@users.noreply.github.com>
2025-07-08 14:52:54 +0530

    Update SearchTask.tsx

--- src/components/SearchTask.tsx
+++ src/components/SearchTask.tsx
@@ -21,7 +21,7 @@ const SearchTask = () => {
 
       setError("");
       // setIsLoading(true);
-      const res = await axios.get("https://taskmanagement-4l0e.onrender.com/api/tasks", {
+      const res = await axios.get("https://taskmanagement-backend-xjgy.onrender.com/api/tasks", {
         headers: {
           Authorization: `Bearer ${token}`
         }

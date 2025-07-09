3e647673c6f7c7c0b0eb9fbd6830595207522f2e
sujithrajendran <67055680+sujithrajendran@users.noreply.github.com>
2025-07-09 15:34:39 +0530

    Update ForgetPassword.tsx

--- src/components/ForgetPassword.tsx
+++ src/components/ForgetPassword.tsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
 import "../css/Auth.css";
+import axios from "axios";
+import { useLoading } from "./LoadingContext";
 
 const ForgotPassword = () => {
+  const { setIsLoading } = useLoading();
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
 
   const handleForgotPassword = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      // simulate API call
+      setIsLoading(true);
+      await axios.post("https://taskmanagement-backend-xjgy.onrender.com/api/tasks/forget-password", {
+        email
+      });
+      setIsLoading(false);
       setMessage("Reset instructions sent to your email");
     } catch {
       setMessage("Failed to send reset email");
@@ -16,19 +23,21 @@ const ForgotPassword = () => {
   };
 
   return (
-    <div className="auth-container">
-      <h2 className="auth-heading">Forgot Password</h2>
-      <form onSubmit={handleForgotPassword} className="auth-form">
-        <input
-          type="email"
-          placeholder="Enter your email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <button type="submit">Send Reset Link</button>
-        {message && <div className="task-alert">{message}</div>}
-      </form>
+    <div className="login-wrapper">
+      <div className="login-container">
+        <h2>Forgot Password</h2>
+        <form onSubmit={handleForgotPassword} className="auth-form">
+          <input
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <button type="submit">Send Reset Link</button>
+          {message && <div className="task-alert">{message}</div>}
+        </form>
+      </div>
     </div>
   );
 };
